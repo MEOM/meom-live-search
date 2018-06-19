bb1caded443bcee12da4280469b4c2135331de25
Janne Leppänen <janne.leppanen@meom.fi>
2018-06-19 12:55:56 +0300

    Add missing letter

--- templates/search-results.php
+++ templates/search-results.php
@@ -1,7 +1,7 @@
 <ul class="meom-live-search__list">
 <?php while ( have_posts() ) : the_post(); ?>
 	<li class="meom-live-search__item">
-		<a class="meom-live-seach__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		<a class="meom-live-search__link" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 	</li>
 <?php endwhile; ?>
 </ul>
